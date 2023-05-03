aa2489da7e96af02fa746e6667761fdadb6aa208
Greg Lowe <18699503+GonzoTG@users.noreply.github.com>
2023-05-03 06:52:53 -0400

    Update initial-setup.sh - Add "net-tools" package

--- initial-setup.sh
+++ initial-setup.sh
@@ -47,7 +47,7 @@ sudo apt update
 sudo apt dist-upgrade -y
 sudo apt -y install nfs-common autofs ntp landscape-client iperf3 cifs-utils \
    smbclient apt-transport-https ca-certificates curl software-properties-common \
-   micro pip
+   micro pip net-tools
 
 if [ "$virt" = "microsoft" ]
 then
